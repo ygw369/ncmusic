8ae738b562c52c77e9ec71de0734405bd5df415d
ygw369 <1831569635@qq.com>
2026-07-03 15:36:52 +0800

    fix: make API base URL configurable via VITE_API_BASE env var

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = import.meta.env.VITE_API_BASE || 'http://localhost:3000'
 
 const instance = axios.create({
   baseURL: BASE_URL,
